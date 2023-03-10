dc123d00f3e6f88be6f5b3c5922d0aeb752c47d6
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-03-11 00:58:52 +0100

    Update main.tsx

--- green/pages/main.tsx
+++ green/pages/main.tsx
@@ -6,7 +6,7 @@ export default function main() {
       <div className='flex flex-col bg-main-green-200 shadow-lg h-[100vh] w-[10vw] text-white'>
         <img src="GreenLogo.jpg" alt="GreenLogo" className='w-[50%] align-top'/>
 
-        <div className=' flex justify-center flex-col align-middle border-2 border-blue-400 h-full'>
+        <div className=' flex justify-center flex-col align-middle h-full'>
           <button className='w-full hover:bg-main-green-100 hover:w-[120%] duration-150 h-10'>
             Map
           </button>
@@ -15,7 +15,7 @@ export default function main() {
           </button>
         </div>
 
-        <button className='w-full hover:bg-red-800 duration-150 border-2 border-green-400'>Log Out</button>
+        <button className='w-full hover:bg-red-800 duration-150'>Log Out</button>
       </div>
     </div>
   )
