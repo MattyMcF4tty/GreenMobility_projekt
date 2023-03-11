9d0bd71781aa513a17680b0bb3911f33b97b47af
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-03-11 19:59:40 +0100

    Map intergration development started

--- green/components/maps.jsx
+++ green/components/maps.jsx
@@ -1,1 +1,10 @@
 import React from "react";
+
+//TODO: https://www.99darshan.com/posts/interactive-maps-using-nextjs-and-google-maps/
+const maps = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
