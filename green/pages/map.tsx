163393d7e13578f364604f4163306876c483652b
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-03-11 19:46:25 +0100

    New file structure

--- green/pages/map.tsx
+++ green/pages/map.tsx
@@ -5,22 +5,7 @@ export default function map() {
   return (
     <div>
       <Navbar />
-      <div className='flex flex-col bg-main-green-200 shadow-lg h-[100vh] w-[10vw] text-white justify-between'>
-        <div className='flex flex-row items-center'>
-          <img src="GreenLogo.jpg" alt="GreenLogo" className='w-[40%] align-top'/>
-        </div>
-
-        <div className='flex flex-col '>
-          <button className='w-full hover:bg-main-green-100 hover:w-[120%] duration-150 h-10'>
-            Map
-          </button>
-          <button className='w-full hover:bg-main-green-100 hover:w-[120%] duration-150 h-10'>
-            Cars
-          </button>
-        </div>
-
-        <button className='w-full hover:bg-red-800 duration-150 h-10'>Log Out</button>
-      </div>
+      
     </div>
   )
 }
