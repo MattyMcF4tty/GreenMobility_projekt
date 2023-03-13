b427cab94e16806dccceea4d007a57b2486e4b4a
Carloslund <carloslundrodriguez@gmail.com>
2023-03-13 12:59:00 +0100

    Register

--- green/components/auth/loginForm.jsx
+++ green/components/auth/loginForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Login = () => {
+const LoginForm = () => {
     return (
     <form className='w-[22rem] flex-col flex justify-center items-center '>
           
@@ -9,13 +9,13 @@ const Login = () => {
         <img src="I7BmDtZqZgXf.png" className="h-32" />
       </div>
       <label htmlFor="text-gray-700">Email:</label>
-      <input className="w-full py-1 bg-gray-50 px--1 outline-none mb-4 focus:bg-gray-200 hover:bg-gray-200 rounded pl-1" type="Email" />
+      <input className="w-full py-1 bg-gray-100 px--1 outline-none mb-4 focus:bg-gray-200 hover:bg-gray-200 rounded pl-1" type="Email" />
       <label htmlFor="text-gray-700">Password:</label>
-      <input className='w-full py-1 bg-gray-50  px--1 outline-none mb-4  focus:bg-gray-200 hover:bg-gray-200 rounded-md pl-1' type="Password" />
+      <input className='w-full py-1 bg-gray-100  px--1 outline-none mb-4  focus:bg-gray-200 hover:bg-gray-200 rounded-md pl-1' type="Password" />
       <input id='remember' className='mb-5' type={'checkbox'} />
       <label htmlFor="remember"> Remember me</label>
       <button className='bg-main-green-100 w-full py-1 rounded' type='submit'>Login</button>
     </div>
   </form>)
 }
-export default Login;
+export default LoginForm;
