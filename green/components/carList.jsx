2aaab01f124337cc8de970dbc26cd055a39d0655
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-03-15 11:48:22 +0100

    sdf

--- green/components/carList.jsx
+++ green/components/carList.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 function CarList () {
     return (
-        <div className="h-full w-full bg-white">
-            
-
-        </div>
+        <table className="w-full h-full">
+            <thead>
+                <tr className="font-semibold">
+                    <th>Car</th>
+                    <th>Location</th>
+                    <th>Energy</th>
+                </tr>
+            </thead>
+        </table>
     
     )
 }
